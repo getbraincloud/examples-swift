259ee68f9118741072f618fde4bfe0e13ce9f9d8
Jonathan McCaffrey <jonmcc.0723@gmail.com>
2018-08-17 14:38:47 -0400

    Additional improvements to the example

--- Basic Example/Basic Example/AppDelegate.swift
+++ Basic Example/Basic Example/AppDelegate.swift
@@ -19,14 +19,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        /*
+         BRAINCLOUD_INFO
+         
+         // We need to create the brainCloud Wrapper
+         AppDelegate._bc = BrainCloudWrapper()
+         
+         // Let's enable debug logging to the console, to help with testing
+         AppDelegate._bc.getBCClient().enableLogging(true)
+         
+         // Now let's pair this brainCloud Client with our app on the brainCloud dashboard
+         
+         // We need to change YOUR_SECRET and YOUR_APPID to match what is on the brainCloud dashboard.
+         // See the readme for more info: https://github.com/getbraincloud/braincloud-objc/blob/master/README.md
+         
+         AppDelegate._bc.initialize("https://sharedprod.braincloudservers.com/dispatcherv2",
+            secretKey: YOUR_SECRET,
+            gameId: YOUR_APPID,
+            gameVersion: "1.0.0",
+            companyName: YOUR_COMPANY,
+            gameName: YOUR_GAME_NAME)
+        */
         
         AppDelegate._bc = BrainCloudWrapper()
+        
         AppDelegate._bc.getBCClient().enableLogging(true)
         
         AppDelegate._bc.initialize("https://sharedprod.braincloudservers.com/dispatcherv2",
-                                  secretKey: "921d9acc-e286-4b37-91b6-a394f4e6ff4f",
-                                  gameId: "12049",
+                                  secretKey: "921d9acc-e286-4b37-91b6-a394f4e6ff4f",    // Replace the Secret and
+                                  gameId: "12049",                                      // AppId with the one on the dashboard
                                   gameVersion: "1.0.0",
                                   companyName: "brainCloud",
                                   gameName: "Basic - Swift")
@@ -35,25 +57,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+
     }
 
 
--- Basic Example/Basic Example/MainScene.swift
+++ Basic Example/Basic Example/MainScene.swift
@@ -4,7 +4,6 @@
 //
 //  Created by brainCloud Support on 2018-08-14.
 //  Copyright © 2018 brainCloud Support. All rights reserved.
-//
 
 import UIKit
 
@@ -12,15 +11,11 @@ class MainScene: UIViewController {
     
     @IBOutlet var test: UIView!
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        print("")
+        super.viewDidLoad()        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     
@@ -35,27 +30,57 @@ class MainScene: UIViewController {
         let jsonData = "{ \"name\": \"\(name)\", \"age\": \"\(age)\" }"
         let jsonAcl = "{ \"other\": 0 }"
         
-        AppDelegate._bc.entityService.createEntity("Person", jsonEntityData: jsonData, jsonEntityAcl: jsonAcl,
-                                                   completionBlock: { (serviceName, serviceOperation, jsonData, cbObject) in
-                                                    print("Success \(String(describing: jsonData))")
-                                                          self.entityLog.text = "\nSuccess \(String(describing: jsonData))"
-                                                    
-                                                    
-                                                    
-                                                    
-        },
-                                                   errorCompletionBlock: { (serviceName, serviceOperation, statusCode, reasonCode, jsonError, cbObject) in
-                                                    print("Failure \(String(describing: serviceName))")
-                                                    
-                                                    self.entityLog.text = "\nFailure \(String(describing: jsonError))"
-                                                    
-                                                    
-                                                    
-        }, cbObject: nil)
+        AppDelegate._bc.entityService.createEntity("Person",
+                                                   jsonEntityData: jsonData,
+                                                   jsonEntityAcl: jsonAcl,
+                                                   completionBlock: onCreateEntity,
+                                                   errorCompletionBlock: onCreateEntityFailed,
+                                                   cbObject: nil)
+        
+    }
     
+    func onCreateEntity(serviceName:String?, serviceOperation:String?, jsonData:String?, cbObject: NSObject?) {
+        print("Success \(String(describing: jsonData))")
+        self.entityLog.text = "\nCreateEntity Success \(String(describing: jsonData))"
+        
+        let data = jsonData?.data(using: String.Encoding.utf8, allowLossyConversion: false)!
+        
+        do {
+            let json = try JSONSerialization.jsonObject(with: data!, options: []) as! [String: AnyObject]
+            let entityIdData = json["data"]?["entityId"] as! String;
+            let entityTypeData = json["data"]?["entityType"] as! String;
+            
+            self.entityId.text = entityIdData;
+            self.entityType.text = entityTypeData;
+        } catch let error as NSError {
+            print("Failed to load: \(error.localizedDescription)")
+        }
+        
+    }
+    
+    func onCreateEntityFailed(serviceName:String?, serviceOperation:String?, statusCode:Int?, reasonCode:Int?, jsonError:String?, cbObject: NSObject?) {
+        self.entityLog.text = "\nCreateEntity Failed \(String(describing: jsonError))"
+        
     }
     
     @IBAction func onDeleteEntity(_ sender: Any) {
+        AppDelegate._bc.entityService.delete(entityId.text,
+                                             version: -1,
+                                             completionBlock: onDeleteEntity,
+                                             errorCompletionBlock: onDeleteEntityFailed,
+                                             cbObject: nil)
+    }
+    
+    func onDeleteEntity(serviceName:String?, serviceOperation:String?, jsonData:String?, cbObject: NSObject?) {
+        
+        self.entityLog.text = "\nDeleteEntity Success \(String(describing: jsonData))"
+        
+        self.entityId.text = "";
+        self.entityType.text = "";
+    }
+    
+    func onDeleteEntityFailed(serviceName:String?, serviceOperation:String?, statusCode:Int?, reasonCode:Int?, jsonError:String?, cbObject: NSObject?) {
+        self.entityLog.text = "\nDeleteEntity Failed \(String(describing: jsonError))"
     }
     
     @IBOutlet weak var entityMenu: UIStackView!
@@ -70,6 +95,5 @@ class MainScene: UIViewController {
             pushNotificationMenu.isHidden = false;
         }
     }
-    
 }
 
