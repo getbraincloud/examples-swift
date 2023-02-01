7b37dd638abd4ffb27d49396b0cded9e25829c61
jo-codegirl <joanneh@bitheads.com>
2023-02-01 14:44:21 -0700

    update build script to launch simulation and exportoptions.plist

--- autobuild/makebuild.sh
+++ autobuild/makebuild.sh
@@ -1,9 +1,41 @@
 #!/bin/bash
 
-#export PROJECTNAME=${1}
-export WORKSPACE=$PWD
+# do this iin root folder > export WORKSPACE=$PWD
+#Eg Usage:
+# ../autobuild/makebuild.sh -list
+# ../autobuild/makebuild.sh -run "Basic Example" C39E0F97-9DA6-41D0-9A95-76A8544BE7CD com.bitheads.Example-Basic
+# ../autobuild/makebuild.sh -pack "Basic Example"
+# ../autobuild/makebuild.sh -run  "brainCloudSwiftUI" 5DF2472F-B32F-4636-9993-4563E0979EDD com.bitheads.brainCloudSwiftUI
+# ../autobuild/makebuild.sh -pack  "brainCloudSwiftUI" 
 
-#xcodebuild -workspace "Basic Example.xcworkspace" -scheme "Basic Example" -destination "platform=iOS Simulator,name=iPhone 8 Plus" 
+# xcworkspace 
+PROJECTNAME=${2}
+# from xcode
+SCHEME=${PROJECTNAME}
+# from list
+DEVICE=${3}
+# from xcode
+BUNDLE=${4}
+SDK='iphoneos16.2'
 
-#xcodebuild -workspace "Basic Example.xcworkspace" -scheme "Basic Example"  -sdk iphoneos16.2  -archivePath "${WORKSPACE}/Build/BasicApp.xcarchive" archive
-xcodebuild -exportArchive -archivePath "${WORKSPACE}/Build/BasicApp.xcarchive" -exportPath "${WORKSPACE}/Build/BasicFolder" -allowProvisioningUpdates -exportOptionsPlist "${WORKSPACE}/ExportOptions.plist"
+if  [ ${1} == -list ]
+then
+	xcrun simctl list
+	xcodebuild -showsdks
+else if  [ ${1} == -run ]
+then
+	# eg. to run in simulator
+	xcodebuild -workspace "${PROJECTNAME}.xcworkspace" -scheme "${SCHEME}" -destination "id=${DEVICE}" SYMROOT="${WORKSPACE}/Build/"
+	xcrun simctl boot ${DEVICE}
+	xcrun simctl install ${DEVICE} "${WORKSPACE}/Build/Debug-iphonesimulator/${PROJECTNAME}.app"
+	xcrun simctl launch ${DEVICE} "${BUNDLE}"
+else if  [ ${1} == -pack ]
+then
+	# to generate .ipa package for ios deployment
+	xcodebuild -workspace "${PROJECTNAME}.xcworkspace" -scheme "${SCHEME}"  -sdk ${SDK}  -archivePath "${WORKSPACE}/Build/${PROJECTNAME}.xcarchive" archive
+	xcodebuild -exportArchive -archivePath "${WORKSPACE}/Build/${PROJECTNAME}.xcarchive" -exportPath "${WORKSPACE}/Build/${PROJECTNAME}Export" -allowProvisioningUpdates -exportOptionsPlist "./ExportOptions.plist"
+else
+	echo Choose -list, -run or -pack.
+fi
+fi
+fi
