ac96dec61ba391d8f189ef4b92a6abea77ab4885
jo-codegirl <joanneh@bitheads.com>
2023-02-03 16:08:10 -0700

    added option to makebuild script

--- autobuild/makebuild.sh
+++ autobuild/makebuild.sh
@@ -18,26 +18,30 @@ DEVICE=${3}
 BUNDLE=com.bitheads.$(echo "${PROJECTNAME}" |  sed 's/ //g')
 SDK='iphoneos16.2'
 
-if  [ ${1} == -list ]
-then
-	xcrun simctl list
-	xcodebuild -showsdks
-else if  [ ${1} == -run ]
-then
-	# eg. to run in simulator
-	xcodebuild -workspace "${PROJECTNAME}.xcworkspace" -scheme "${SCHEME}" -destination "id=${DEVICE}" SYMROOT="${WORKSPACE}/Build/"
-	xcrun simctl shutdown ${DEVICE}
-	xcrun simctl erase ${DEVICE}
-	xcrun simctl boot ${DEVICE}
-	xcrun simctl install ${DEVICE} "${WORKSPACE}/Build/Debug-iphonesimulator/${PROJECTNAME}.app"
-	xcrun simctl launch ${DEVICE} "${BUNDLE}"
-else if  [ ${1} == -pack ]
-then
-	# to generate .ipa package for ios deployment
-	xcodebuild -workspace "${PROJECTNAME}.xcworkspace" -scheme "${SCHEME}"  -sdk ${SDK}  -archivePath "${WORKSPACE}/Build/${PROJECTNAME}.xcarchive" archive
-	xcodebuild -exportArchive -archivePath "${WORKSPACE}/Build/${PROJECTNAME}.xcarchive" -exportPath "${WORKSPACE}/Build/${PROJECTNAME}-Export" -allowProvisioningUpdates -exportOptionsPlist "./ExportOptions.plist"
-else
-	echo Choose -list, -run or -pack.
-fi
-fi
-fi
+case "$1" in
+    -list)
+		xcodebuild -showsdks
+		;;
+	-build)
+		xcodebuild -workspace "${PROJECTNAME}.xcworkspace" -scheme "${SCHEME}" -destination "$DEVICE" SYMROOT="${WORKSPACE}/Build/"
+		;;
+ 	-run)
+		# eg. to run in simulator
+		xcodebuild -workspace "${PROJECTNAME}.xcworkspace" -scheme "${SCHEME}" -destination "id=${DEVICE}" SYMROOT="${WORKSPACE}/Build/"
+		xcrun simctl shutdown ${DEVICE}
+		xcrun simctl erase ${DEVICE}
+		xcrun simctl boot ${DEVICE}
+		xcrun simctl install ${DEVICE} "${WORKSPACE}/Build/Debug-iphonesimulator/${PROJECTNAME}.app"
+		xcrun simctl launch ${DEVICE} "${BUNDLE}"
+		;;
+	-pack)
+		# to generate .ipa package for ios deployment
+		xcodebuild -workspace "${PROJECTNAME}.xcworkspace" -scheme "${SCHEME}"  -sdk ${SDK}  -archivePath "${WORKSPACE}/Build/${PROJECTNAME}.xcarchive" archive
+		xcodebuild -exportArchive -archivePath "${WORKSPACE}/Build/${PROJECTNAME}.xcarchive" -exportPath "${WORKSPACE}/Build/${PROJECTNAME}-Export" -allowProvisioningUpdates -exportOptionsPlist "./ExportOptions.plist"
+		;;
+	 *)
+        echo Choose -list, -run or -pack.
+        exit 1
+ 
+esac
+exit 0
